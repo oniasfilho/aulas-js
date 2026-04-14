3823affa2ee76f642ffa7e3f0b0874f4996ebf7c
Onias da Rocha Filho <onias.filho@gmail.com>
2026-04-14 21:55:19 +0000

    primeira aula

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const firstName = "Joyce";
-const lastName = "Rocha";
+// variaveis
+var idade = 31;
+let sobrenome = "da Rocha Filho";
+const nome = "Onias";
+var nomeCompleto;
 
-function mergeName(name, lastName) {
-  const fullName = name + lastName;
-  return fullName;
+// função
+function obtemNomeCompleto(primeiroParametro, segundoParametro) {
+    console.log("Acabou de chamar a função obtemNomeCompleto")
+    return primeiroParametro + " " + segundoParametro;
 }
 
-const nomeGerado = mergeName(firstName, lastName);
+nomeCompleto = obtemNomeCompleto(nome, sobrenome);
 
-console.log(nomeGerado);
+console.log(nomeCompleto)
+
+var resultado;
+
+function soma(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+resultado = soma(5, 5);
+
+console.log(resultado);
+
+
+// EXERCICIO DO DIA
+// - Store a user name and age
+// - Print user information
+// - Create a simple calculator (soma, divisao, multiplicacao)
+
+
+//FOCOS -> variaveis e funcao
+// VARIAVEL -> ARMAZENA DADO
+// FUNCAO -> recebe parametros (variaveis) e devolve algo (variavel)
