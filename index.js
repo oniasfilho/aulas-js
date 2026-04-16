35732ff08aef7dcde6151431872d02f6f950415e
joyce-rocha <joycerck@hotmail.com>
2026-04-16 07:55:32 -0300

    tarefinha feita s2

--- index.js
+++ index.js
@@ -24,6 +24,63 @@ resultado = soma(5, 5);
 
 console.log(resultado);
 
+let studentName;
+let studentAge;
+let profAge = 31
+ 
+function studentInformation(studentName, studentAge) {
+console.log("O(a) aluno(a) se chama " + studentName + ", e tem " + studentAge + " anos.")
+    if (studentAge < profAge) {
+        console.log("Aluno(a) xovem")
+    }else{
+        console.log("Aluno(a) véio(a)")
+    }
+}
+
+studentInformation("Joyce", 35);
+
+let number1;
+let number2;
+
+function adicao(number1, number2) {
+    let adicao = number1 + number2;
+    console.log("O resultado da soma de " + number1 + " + " + number2 + " é " + adicao);
+}
+function subtracao(number1, number2) {
+    let subtracao = number1 - number2;
+    console.log("O resultado da subtração de " + number1 + " - " + number2 + " é " + subtracao);
+}
+function multiplicacao(number1, number2) {
+    let multiplicacao = number1 * number2;
+    console.log("O resultado da multiplicação de " + number1 + " x " + number2 + " é " + multiplicacao);
+}
+function divisao(number1, number2) {
+    let divisao = number1 / number2;
+    console.log("O resultado da divisão de " + number1 + " / " + number2 + " é " + divisao);
+}
+function resto(number1, number2) {
+    let resto = number1 % number2;
+    console.log("O resultado do resto da divisão de " + number1 + " / " + number2 + " é " + resto);
+}
+
+
+adicao(2, 2);
+subtracao(10, 7);
+multiplicacao(6, 6);
+divisao(10, 2)
+resto(100, 30);
+
+let familia = [  {papel: "pai", nome : "Onias", anoNasc : 1943},
+    {papel: "mãe", nome : "Elenil", anoNasc : 1971},
+    {papel: "filha", nome : "Joyce", anoNasc : 1990},
+    {papel: "filho", nome : "Onias F", anoNasc : 1995}]
+      
+    if (familia[2].anoNasc < 2007 && familia[3].anoNasc < 2007) {
+        console.log("Os filhos do Prof Onias e Elenil são todos maiores de idade")
+    } else {console.log("Prof Onias e Elenil têm filhos menores de idade")}
+  
+
+
 
 // EXERCICIO DO DIA
 // - Store a user name and age
